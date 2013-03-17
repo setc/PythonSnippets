19dc2f3c88485d10320bb7c1f0b9f4373ff9d087
Sebastian Torrente <sebastian.torrente@gmail.com>
2013-03-17 18:25:14 +0000

    Added __str__ method

--- OOP/Persons_and_Managers/person.py
+++ OOP/Persons_and_Managers/person.py
@@ -5,7 +5,7 @@
 #Something good to have at hand as a reference
 
 """
-This module contain the person class.
+This module contain the Person class.
 """
 
 class Person:
@@ -19,6 +19,12 @@ class Person:
         self.name = name
         self.job  = job
         self.pay  = pay
+
+    def __str__(self):
+        """
+        Format: [Name: John Doe, Job: Barbarian, Pay: 5000]
+        """
+        return '[Name: %s, Job: %s, Pay: %s]' % (self.name, self.job, self.pay)
 
     def firstName(self):
         """
@@ -41,9 +47,12 @@ class Person:
 if __name__ == '__main__':                              #Test de class
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job = 'dev', pay = 100000)
-    print(bob.name," current pay is: ", bob.pay)
-    print(sue.name," current pay is: ", sue.pay)
+#    print(bob.name," current pay is: ", bob.pay)
+#    print(sue.name," current pay is: ", sue.pay)
+    print(bob)
+    print(sue)
     print("Bob Smit First Name is: ", bob.firstName())
     print("Sue Jones Last Name is: ", sue.lastName())
     sue.giveRaise(.10)
-    print("After a raise of 10% sue earns ",sue.pay)
+#    print("After a raise of 10% sue earns ",sue.pay)
+    print(sue)
