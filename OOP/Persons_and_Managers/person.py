ca7fbde93cba7a3beb2ddd118fafb2edd1e5ad30
Sebastian Torrente <sebastian.torrente@gmail.com>
2013-03-17 18:14:30 +0000

    Added more methods for names and raises, also some docstring and an additional method of my own

--- OOP/Persons_and_Managers/person.py
+++ OOP/Persons_and_Managers/person.py
@@ -19,9 +19,31 @@ class Person:
         self.name = name
         self.job  = job
         self.pay  = pay
+
+    def firstName(self):
+        """
+        Return the first name of the employee.
+        """
+        return self.name.split()[0]
+
+    def lastName(self):
+        """
+        Return the last name of the employee.
+        """
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        """
+        Update the pay by adding a raise based on percentage
+        """
+        self.pay = int(self.pay * (1 + percent))
         
 if __name__ == '__main__':                              #Test de class
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job = 'dev', pay = 100000)
-    print(bob.name, bob.pay)
-    print(sue.name, sue.pay)
+    print(bob.name," current pay is: ", bob.pay)
+    print(sue.name," current pay is: ", sue.pay)
+    print("Bob Smit First Name is: ", bob.firstName())
+    print("Sue Jones Last Name is: ", sue.lastName())
+    sue.giveRaise(.10)
+    print("After a raise of 10% sue earns ",sue.pay)
