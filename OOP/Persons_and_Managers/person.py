43782e0d8ceda4d61ae4027e2fc525291370cca2
Sebastian Torrente <sebastian.torrente@gmail.com>
2013-03-17 17:55:10 +0000

    Added some person and managers oop example

---
+++ OOP/Persons_and_Managers/person.py
@@ -0,0 +1,12 @@
+#person.py
+#Advanced Persons & Managers
+#
+#This is the class example in Learn Python with employees and managers.
+#Something good to have at hand as a reference
+
+class Person:
+    def __init__(self, name, job, pay):
+        self.name = name
+        self.job  = job
+        self.pay  = pay
+        
