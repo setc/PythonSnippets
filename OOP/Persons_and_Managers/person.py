bb8eca0d93e5a81ed9fb717910ea4f6134c0f272
Sebastian Torrente <sebastian.torrente@gmail.com>
2013-03-17 17:58:40 +0000

    Spicing up things a little and added docstrings

--- OOP/Persons_and_Managers/person.py
+++ OOP/Persons_and_Managers/person.py
@@ -4,8 +4,18 @@
 #This is the class example in Learn Python with employees and managers.
 #Something good to have at hand as a reference
 
+"""
+This module contain the person class.
+"""
+
 class Person:
-    def __init__(self, name, job, pay):
+    def __init__(self, name, job = None, pay = 0):
+        """
+        Initiate the class. The three fields are:
+        name: Name of the employee
+        job : His/her job
+        pay : How much it gains per month
+        """
         self.name = name
         self.job  = job
         self.pay  = pay
